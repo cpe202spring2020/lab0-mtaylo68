c65065a027eaa71769e4a9f9501f8bd876b5ced2
Mary Kathryn Taylor <marykathryntaylor@Marys-MacBook-Air-3.local>
2020-04-06 15:49:05 -0400

    wrote code

--- planets.py
+++ planets.py
@@ -1,6 +1,8 @@
 def weight_on_planets():
-   # write your code here
-   
+   weight = float(input("What do you weigh on earth? "))
+   mars = weight * 0.38
+   jupiter = weight * 2.34
+   print('\nOn Mars you would weigh', mars, 'pounds.\nOn Jupiter you would weigh', jupiter, 'pounds.')
    
    
 if __name__ == '__main__':
--- planets_tests.py
+++ planets_tests.py
@@ -13,5 +13,14 @@ class Test_planets(unittest.TestCase):
         self.assertEqual(expected_out, student_output.getvalue().strip())
 
 
+    def test_02(self):
+        sys.stdin = io.StringIO("0")
+        sys.stdout = student_output = io.StringIO()        
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 0.0 pounds.\nOn Jupiter you would weigh 0.0 pounds."
+        weight_on_planets()
+        #print(student_output.getvalue().strip())
+        self.assertEqual(expected_out, student_output.getvalue().strip())
+
+
 if __name__ == "__main__":
         unittest.main()
